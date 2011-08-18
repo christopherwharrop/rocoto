677c4ffb5dffbbf7ab2254232395a8a92bd4895f
christopher.w.harrop <christopher.w.harrop@958a110a-9645-0410-8839-e42eaa69150f>
2011-08-18 18:16:30 +0000

    Added two basic RelaxNG schemas for validating XML.  They only validate the <workflow> tag and its attributes so far. Added code to parse XML workflow documents.  The entire XML document is being parsed, but no <metatask> tags are being expanded yet. And no objects are looking at the XML document tree yet.

--- lib/workflowmgr/workflowdoc.rb
+++ lib/workflowmgr/workflowdoc.rb
@@ -12,6 +12,8 @@ module WorkflowMgr
   ##########################################
   class WorkflowXMLDoc
 
+    require 'libxml'
+
     ##########################################
     #
     # initialize
@@ -19,9 +21,78 @@ module WorkflowMgr
     ##########################################
     def initialize(workflowdoc)
 
-      
+      @xmlfile=workflowdoc
+
+      # Get the text from the xml file and put it into a string
+      @xmlstring=WorkflowMgr.forkit(2) do
+        IO.readlines(@xmlfile,nil)[0]
+      end
+
+      # Parse the workflow xml string, set option to replace entities
+      @workflowdoc=LibXML::XML::Document.string(@xmlstring,:options => LibXML::XML::Parser::Options::NOENT)
+
+      # Validate the workflow xml document before metatask expansion
+      validate_with_metatasks
+
+      # Expand metatasks
+      expand_metatasks
+
+      # Validate the workflow xml document after metatask expansion
+      # The second validation is needed in case metatask expansion introduced invalid XML
+      validate_without_metatasks
 
     end  # initialize
+
+  private
+
+
+    ##########################################
+    #
+    # validate_with_metatasks
+    #
+    ##########################################
+    def validate_with_metatasks
+
+      # Parse the Relax NG schema XML document
+      relaxng_document = LibXML::XML::Document.file("#{File.dirname(__FILE__)}/schema_with_metatasks.rng")
+
+      # Prepare the Relax NG schemas for validation
+      relaxng_schema = LibXML::XML::RelaxNG.document(relaxng_document)
+
+      # Validate the workflow XML file against the general Relax NG Schema that validates metatask tags
+      @workflowdoc.validate_relaxng(relaxng_schema)
+
+    end
+
+
+    ##########################################
+    #
+    # validate_with_metatasks
+    #
+    ##########################################
+    def validate_without_metatasks
+
+      # Parse the Relax NG schema XML document ("_m" means it validates metatask tags)
+      relaxng_document = LibXML::XML::Document.file("#{File.dirname(__FILE__)}/schema_without_metatasks.rng")
+
+      # Prepare the Relax NG schemas for validation
+      relaxng_schema = LibXML::XML::RelaxNG.document(relaxng_document)
+
+      # Validate the workflow XML file against the general Relax NG Schema that validates metatask tags
+      @workflowdoc.validate_relaxng(relaxng_schema)
+
+    end
+
+
+    ##########################################
+    #
+    # validate_with_metatasks
+    #
+    ##########################################
+    def expand_metatasks
+
+    end
+
 
   end  # Class WorkflowXMLDoc
 
--- lib/workflowmgr/workflowengine.rb
+++ lib/workflowmgr/workflowengine.rb
@@ -24,10 +24,11 @@ module WorkflowMgr
     ##########################################
     def initialize(args)
 
+      # Get command line options
       @options=WorkflowOption.new(args)
+
+      # Get configuration file options
       @config=WorkflowYAMLConfig.new
-      @workflowdoc=WorkflowXMLDoc.new(@options.workflowdoc)
-      @workflowdb=WorkflowSQLite3DB.new(@options.database)
 
     end  # initialize
 
@@ -40,9 +41,21 @@ module WorkflowMgr
     def run
 
       begin
+
+        # Initialize the database
+        @workflowdb=WorkflowSQLite3DB.new(@options.database)
+
+        # Acquire a lock on the workflow in the database
         @workflowdb.lock_workflow
+
+        # Initialize the workflow document
+        @workflowdoc=WorkflowXMLDoc.new(@options.workflowdoc)
+
       ensure
+
+        # Make sure we release the workflow lock in the database
         @workflowdb.unlock_workflow
+
       end
  
     end  # run
