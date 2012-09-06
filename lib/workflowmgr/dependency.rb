e3812f5367e6783eebd6b1789c6ad7c9319ffbac
christopher.w.harrop <christopher.w.harrop@958a110a-9645-0410-8839-e42eaa69150f>
2012-09-06 18:49:33 +0000

    Add query methods to dependencies to return text describing status of dependency nodes

--- lib/workflowmgr/dependency.rb
+++ lib/workflowmgr/dependency.rb
@@ -41,6 +41,22 @@ module WorkflowMgr
 
     end
 
+    #####################################################
+    #
+    # Query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      begin
+        return(@root.query(cycle,jobList,workflowIOServer))
+      rescue WorkflowIOHang
+        puts $!
+        return false
+      end
+
+    end
+
   end
 
 
@@ -74,6 +90,23 @@ module WorkflowMgr
       return !@operand.resolved?(cycle,jobList,workflowIOServer)
 
     end
+
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      query=@operand.resolved?(cycle,jobList,workflowIOServer)
+      if query.first[:resolved]
+        return [{:dep=>"NOT", :msg=>"is not satisfied", :resolved=>false }, query]
+      else
+        return [{:dep=>"NOT", :msg=>"is satisfied", :resolved=>true }, query]
+      end
+
+    end
+
 
   end
 
@@ -110,6 +143,25 @@ module WorkflowMgr
 
     end
 
+
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      @operands.each { |operand|
+        query = operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        return [{:dep=>"AND", :msg=>"is not satisfied", :resolved=>false }, queries] unless query.first[:resolved]
+      }
+      return [{:dep=>"AND", :msg=>"is satisfied", :resolved=>true }, queries]
+
+    end
+
+
   end
 
 
@@ -144,6 +196,25 @@ module WorkflowMgr
       return false
 
     end
+
+
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      @operands.each { |operand|
+        query = operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        return [{:dep=>"OR", :msg=>"is satisfied", :resolved=>true }, queries] if query.first[:resolved]
+      }
+      return [{:dep=>"OR", :msg=>"is not satisfied", :resolved=>false }, queries]
+
+    end
+
 
   end
 
@@ -180,6 +251,23 @@ module WorkflowMgr
 
     end
 
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      @operands.each { |operand|
+        query = operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        return [{:dep=>"<nand>", :msg=>"is satisfied", :resolved=>true }, queries] if !query.first[:resolved]
+      }
+      return [{:dep=>"<nand>", :msg=>"is not satisfied", :resolved=>false }, queries]
+
+    end
+
   end
 
   ##########################################
@@ -211,6 +299,23 @@ module WorkflowMgr
         return false if operand.resolved?(cycle,jobList,workflowIOServer)
       }
       return true
+
+    end
+
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      @operands.each { |operand|
+        query=operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        return [{:dep=>"NOR", :msg=>"is not satisfied", :resolved=>false }, queries] if query.first[:resolved]
+      }
+      return [{:dep=>"NOR", :msg=>"is satisfied", :resolved=>true }, queries]
 
     end
 
@@ -250,6 +355,29 @@ module WorkflowMgr
 
     end
 
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      ntrue=0
+      @operands.each { |operand|
+        query = operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        ntrue += 1 if query.first.resolved?
+        return [{:dep=>"XOR", :msg=>"is not satisfied", :resolved=>false }, queries] if ntrue > 1
+      }
+      if ntrue==1
+        return [{:dep=>"XOR", :msg=>"is satisfied", :resolved=>true }, queries]
+      else
+        return [{:dep=>"XOR", :msg=>"is not satisfied", :resolved=>false }, queries]
+      end
+ 
+    end
+
   end
 
   ##########################################
@@ -287,6 +415,26 @@ module WorkflowMgr
 
     end
 
+    #####################################################
+    #
+    # query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      queries=[]
+      ntrue=0.0
+      @operands.each { |operand|
+        query = operand.query(cycle,jobList,workflowIOServer)
+        queries += query
+        ntrue += 1.0 if query.first[:resolved]
+        return [{:dep=>"SOME", :msg=>"is satisfied", :resolved=>true }, queries] if ntrue/@operands.size >= @threshold
+
+      }
+      return [{:dep=>"SOME", :msg=>"is not satisfied", :resolved=>false }, queries]
+
+    end
+
   end
 
 
@@ -320,8 +468,25 @@ module WorkflowMgr
       return false if jobList.nil?      
       return false if jobList[@task].nil?
       return false if jobList[@task][cycle.getgm+@cycle_offset].nil?
-      return jobList[@task][cycle.getgm+@cycle_offset][:state]==@state
+      return jobList[@task][cycle.getgm+@cycle_offset].state==@state
 
+    end
+
+    #####################################################
+    #
+    # Query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      return [{:dep=>"#{@task} of cycle #{cycle.getgm+@cycle_offset}", :msg=>"is not #{@state}", :resolved=>false }] if jobList.nil?
+      return [{:dep=>"#{@task} of cycle #{cycle.getgm+@cycle_offset}", :msg=>"is not #{@state}", :resolved=>false }] if jobList[@task].nil?
+      return [{:dep=>"#{@task} of cycle #{cycle.getgm+@cycle_offset}", :msg=>"is not #{@state}", :resolved=>false }] if jobList[@task][cycle.getgm+@cycle_offset].nil?
+      if jobList[@task][cycle.getgm+@cycle_offset].state==@state
+        return [{:dep=>"#{@task} of cycle #{cycle.getgm+@cycle_offset}", :msg=>"is #{@state}", :resolved=>true }]
+      else
+        return [{:dep=>"#{@task} of cycle #{cycle.getgm+@cycle_offset}", :msg=>"is not #{@state}", :resolved=>false }]
+      end
     end
 
   end
@@ -353,13 +518,38 @@ module WorkflowMgr
     #####################################################
     def resolved?(cycle,jobList,workflowIOServer)
 
-      t=@timestr.to_s(cycle)      
-      return Time.now.getgm > Time.gm(t[0..3],
-                                      t[4..5],
-                                      t[6..7],
-                                      t[8..9],
-                                      t[10..11],
-                                      t[12..13])
+      timestr=@timestr.to_s(cycle)
+      t=Time.gm(timestr[0..3],
+                timestr[4..5],
+                timestr[6..7],
+                timestr[8..9],
+                timestr[10..11],
+                timestr[12..13])
+
+      return Time.now.getgm > t
+
+     end    
+
+    #####################################################
+    #
+    # Query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      timestr=@timestr.to_s(cycle)
+      t=Time.gm(timestr[0..3],
+                timestr[4..5],
+                timestr[6..7],
+                timestr[8..9],
+                timestr[10..11],
+                timestr[12..13])
+      
+      if Time.now.getgm > t
+        return [{:dep=>"Walltime", :msg=>"is > #{t}", :resolved=>true }]
+      else
+        return [{:dep=>"Walltime", :msg=>"is <= #{t}", :resolved=>false }]
+      end
     
     end
 
@@ -399,6 +589,27 @@ module WorkflowMgr
       end
 
     end
+
+    #####################################################
+    #
+    # Query
+    #
+    #####################################################
+    def query(cycle,jobList,workflowIOServer)
+
+      filename=@datapath.to_s(cycle)
+      if workflowIOServer.exists?(filename)
+        if Time.now > (workflowIOServer.mtime(filename) + @age)
+          return [{:dep=>filename, :msg=>"is available", :resolved=>true }]
+        else
+          return [{:dep=>filename, :msg=>"is not old enough", :resolved=>false }]
+        end
+      else
+        return [{:dep=>filename, :msg=>"does not exist", :resolved=>false }]
+      end
+
+    end
+
 
   end
 
