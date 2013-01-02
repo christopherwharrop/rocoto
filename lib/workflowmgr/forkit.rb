d132f33d59451f2d9f1d13c69820752f8365f3bf
christopher.w.harrop <christopher.w.harrop@958a110a-9645-0410-8839-e42eaa69150f>
2013-01-02 17:34:24 +0000

    Cleanup:  Remove dead code that is not longer used.

--- lib/workflowmgr/forkit.rb
+++ lib/workflowmgr/forkit.rb
@@ -106,19 +106,4 @@ module WorkflowMgr
 
   end
 
-
-  ##########################################
-  #
-  # WorkflowMgr.cmderr(cmd)
-  #
-  ##########################################
-  def WorkflowMgr.cmderr(cmd)
-
-    STDERR.puts
-    STDERR.puts "ERROR: The command '#{cmd}' timed out."
-    STDERR.puts "       The process may have hung due to an unresponsive server or may be due to heavy load"
-    STDERR.puts
-
-  end
-
 end  # module workflowmgr
