6f7ee433488038237044ce741df0ece5d8f99320
christopher.w.harrop <christopher.w.harrop@958a110a-9645-0410-8839-e42eaa69150f>
2013-02-04 20:22:37 +0000

    Bug Fix:
    Many thanks to Tim Brown for this brilliant idea!
    
    This commit eliminates the use of /tmp (or any filesystem) for retrieving
    the URI of DRb rocoto server processes.  In the process, it fixes bugs
    related to the accidental reuse of /tmp/rocoto_uri_* files that caused
    file permission problems and sometimes resulted in Rocoto trying to 
    connect to the wrong URI.  It also fixes a bug that resulted in the 
    workflowIOProxy looking at the wrong rocotoioserver PID when attempting
    to recover from IO hangs (the daemonization process resulted in the use
    of the wrong pid).
    
    The main Rocoto process now opens a pipe before forking the processes
    that daemonize themselves and become the rocoto*server processes.  The
    children inheret the file descriptors for these pipes, so they can be
    used on both ends to communicate the URI and the eventual server PID.
    The forked server process simply opens the inherited write end of the pipe
    and writes the URI and its PID (it simply closes the read end of the pipe).
    Meanwhile the main Rocoto process reads the read end of the pipe, and 
    retrieves the URI and the pid, enabling it to connect to the DRb Rocoto
    server process (it simply closes the write end of the pipe).
    
    Thus, the filesystem is no longer used for communications between processes.
    
    This does need to be more rigorously tested under a more realistic workflow
    load and adverse operating conditions, but it is working reliably in a
    light testing environment.

--- lib/workflowmgr/launchserver.rb
+++ lib/workflowmgr/launchserver.rb
@@ -18,64 +18,60 @@ module WorkflowMgr
   ##########################################
   def WorkflowMgr.launchServer(server)
 
-    # Fork a child process that will start the server process
+    # Open a pipe for retrieving the URI and pid of the DRb server process
+    rd,wr=IO.pipe
+
+    # Get the pid of this process
     parent_pid=Process.pid
-    server_pid = fork
-    if server_pid.nil?
+
+    # Fork a child process that will daemonize itself and start the server process
+    child_pid = fork
+    unless child_pid
+
+      # Immediately close the read end of the pipe because the child is only going to write
+      rd.close
 
       # This is the child process, so exec the server command
-      # Tell the server the pid of the process that is launching it
-      exec("#{server} #{parent_pid} #{WorkflowMgr::VERBOSE}")
-
-    else     
-
-      # The server process will daemonize itself.  That will cause the
-      # child process we just forked to exit.  We  must harvest the status
-      # of the child we just forked to avoid accumulcation of zombie processes.
-      Process.waitpid(server_pid)
-
-      # This is the parent process, so retrieve the URI of the forked server
-      uri=""
-      uri_file="#{Dir.tmpdir}/rocoto_uri_#{server_pid}"
-
-      # Read URI of server from a file in /tmp
-      begin
-
-        # Wait for the uri file to become available, then read it
-        SystemTimer.timeout(10) do
-          while !File.exists?(uri_file) do
-            sleep 0.25 
-          end
-          file=File.new(uri_file)
-          uri=file.gets
-          while uri.nil? do
-            sleep 0.25
-            uri=file.gets
-          end
-          file.close
-          
-        end
-
-      rescue Timeout::Error
-      
-        # The uri file could not be found, so we cannot contact the server.
-	# Either it died before it could write the URI file, or the file disappeared
-        # The server process will shut itself down once it detects that this process
-        # has terminated.
-        WorkflowMgr.log("Could not find URI of #{File.basename(server)} in #{uri_file}.  Either the server crashed at startup, or the #{Dir.tmpdir} filesystem is misbehaving.")
-        raise "Could not find URI of #{File.basename(server)} in #{uri_file}"
-
-      ensure
-
-        # Make sure to remove the temporary URI file after we have read it
-        File.delete(uri_file) unless !File.exists?(uri_file)
-
-      end
-
-      # Connect to the server process and return the object being served
-      return [ DRbObject.new(nil,uri), Socket::getaddrinfo(Socket.gethostname, nil, nil, Socket::SOCK_STREAM)[0][3], server_pid ]
+      # Pass the server the pid of the process that is launching it, the verbosity level, 
+      # and the file descriptor to use for sending the URI back to the parent
+      exec("#{server} #{parent_pid} #{WorkflowMgr::VERBOSE} #{wr.fileno}")
 
     end
+
+    # Immediately close the write end of the pipe because we are only going to read
+    wr.close
+
+    # The server process will daemonize itself.  That will cause the
+    # child process we just forked to exit.  We  must reap the status
+    # of the child we just forked to avoid accumulcation of zombie processes.
+    Process.waitpid(child_pid)
+
+    # Initialize the URI and pid of the server process
+    uri=""
+    server_pid=0
+
+    begin
+
+      # Read the URI and pid of the server from the read end of the pipe we just created
+      SystemTimer.timeout(10) do
+        uri=rd.gets
+        server_pid=rd.gets
+        rd.close
+      end
+
+    rescue Timeout::Error
+      
+      # The URI file could not be read, so we cannot contact the server.
+      # Either the server process died before it could write the URI and pid
+      # or there was some sort of network problem.# The server process will 
+      # shut itself down once it detects that this process has terminated.
+      WorkflowMgr.log("Never received the URI and/or pid of #{File.basename(server)}.")
+      raise "Never the received URI and/or pid of #{File.basename(server)}."
+
+    end
+
+    # Connect to the server process and return the object being served
+    return [ DRbObject.new(nil,uri), Socket::getaddrinfo(Socket.gethostname, nil, nil, Socket::SOCK_STREAM)[0][3], server_pid ]
 
   end
 
