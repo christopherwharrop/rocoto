91182b42b97ddbf0addb5aadcef10249505d7f99
Christopher Harrop <christopher.w.harrop@noaa.gov>
2017-08-23 16:51:27 +0000

    Add new cycletime flags to denote new date components.
    @n = Number of days in the month
    @o = All lower case abbreviated month name
    @O = All lower case full month name
    
    Special handling of these non-standard flags was needed.

--- lib/workflowmgr/cyclestring.rb
+++ lib/workflowmgr/cyclestring.rb
@@ -5,6 +5,8 @@
 ##########################################
 module WorkflowMgr
 
+  require 'date'
+
   ########################################## 
   #
   # Class CycleString
@@ -34,7 +36,23 @@ module WorkflowMgr
     #####################################################
     def to_s(cycle)
 
-      return (cycle.gmtime+@offset).strftime(@str)
+      # Calculate the reference time
+      reftime = cycle.gmtime+@offset
+
+      # Compute non-standard date/time components
+      days_in_month = Date.new(reftime.year, reftime.month, -1).day
+      lower_case_month_abbr = reftime.strftime("%b").downcase
+      lower_case_month_full = reftime.strftime("%B").downcase
+
+      # Take care of non-standard flags first
+      str = @str.gsub("%n","#{days_in_month}")
+      str = str.gsub("%o","#{lower_case_month_abbr}")
+      str = str.gsub("%O","#{lower_case_month_full}")
+
+      # Process standard flags
+      str = reftime.strftime(str)
+
+      return (str)
 
     end
 
