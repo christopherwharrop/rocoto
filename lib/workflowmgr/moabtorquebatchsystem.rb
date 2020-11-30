7b3f39b0da9084272806eacaba0cfd9fd4bc3bf9
Christopher Harrop <christopherwharrop@users.noreply.github.com>
2020-11-30 09:45:45 -0700

    Add missing config argument during initialization of moabtorque (#86)
    batch system.
    
    Co-authored-by: Christopher Harrop <Christopher.W.Harrop@noaa.gov>

--- lib/workflowmgr/moabtorquebatchsystem.rb
+++ lib/workflowmgr/moabtorquebatchsystem.rb
@@ -32,7 +32,7 @@ module WorkflowMgr
       @showq_c_timeout=config.JobAcctTimeout
 
       # Initialize a Torque batch system object
-      @torque=TORQUEBatchSystem.new
+      @torque=TORQUEBatchSystem.new(config)
 
       # Initialize an empty hash for job queue records
       @jobqueue={}
