79304a1c47a18ee68c45a52799935c481d0d6d56
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2025-10-16 11:29:19 -0600

    Revert "Allow `-n | --dryrun` for submission rocoto commands (#117)" (#119)
    This reverts commit 438143b51226330ea05b150b810fd7e2e1f93062.

--- lib/workflowmgr/pbsprobatchsystem.rb
+++ lib/workflowmgr/pbsprobatchsystem.rb
@@ -217,11 +217,7 @@ module WorkflowMgr
       WorkflowMgr.stderr("Submitting #{task.attributes[:name]} using #{cmd} < #{tf.path} with input {{#{input}}}",4)
 
       # Run the submit command
-      if WorkflowMgr::DRYRUN > 0
-        output="This is a dryrun"
-      else
-        output=`#{cmd} < #{tf.path} 2>&1`.chomp()
-      end
+      output=`#{cmd} < #{tf.path} 2>&1`.chomp()
 
       # Parse the output of the submit command
       if output=~/^(\d+)(\.[a-zA-Z0-9-]+)*$/
