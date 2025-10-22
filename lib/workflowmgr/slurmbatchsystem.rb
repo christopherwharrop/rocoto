6abe071d7b225955d1e82bf92fb1ad00d21ec6d8
TerrenceMcGuinness-NOAA <terry.mcguinness@noaa.gov>
2025-10-22 13:26:12 -0400

    rolled back redundent check for dryrun in the elseif of output from the WorkflowMgr.dryrun_mode update

--- lib/workflowmgr/slurmbatchsystem.rb
+++ lib/workflowmgr/slurmbatchsystem.rb
@@ -321,8 +321,6 @@ module WorkflowMgr
       # Parse the output of the submit command
       if output=~/^Submitted batch job (\d+)/
         return $1,output
-      elsif output=~/^This is a dryrun/
-        return nil,output
       elsif output=~/Batch job submission failed: Socket timed out/
 
         WorkflowMgr.stderr("WARNING: '#{output}', looking to see if job was submitted anyway...", 1)
